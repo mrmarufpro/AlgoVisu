93362cc13f2ba9c96a21db28612d467e16ed5b7a
Ubuntu <azureuser@OpenClaw.0yqubj2cc0uepn3mh5uzyyaefg.ix.internal.cloudapp.net>
2026-03-22 16:53:41 +0000

    chore: address Copilot review feedback

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { SortingContextProvider } from 'Context/SortingContext';
 import BubbleSortContextProvider from 'Context/BubbleSortContext';
--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import Wrapper from 'components/Wrapper';
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error("Root container with id 'root' not found");
+}
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
